d216bbae6b76cc5682aa713a2a2b953c845a357e
Anuradha <anuradhaah667@gmail.com>
2024-02-04 18:17:21 +0530

    edit code

---
+++ Airline_Reservation_System.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.airline_reservation_system;
+
+/**
+ *
+ * @author anura
+ */
+public class Airline_Reservation_System {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
